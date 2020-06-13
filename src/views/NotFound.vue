3f50f7a05bb386e3eebb803681430aa289d7f43c
dmitri korchemkin <dmitri.korchemkin@gmail.com>
2020-06-14 02:01:00 +0300

    update 404 page; remove files;

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,5 +1,24 @@
 <template>
-  <div>
-    404
-  </div>
+  <v-card
+    flat
+    outlined
+  >
+    <v-img
+      class="white--text align-end"
+      height="200px"
+      src="@/assets/docks.jpg"
+    >
+      <v-card-title>404</v-card-title>
+    </v-img>
+    <v-card-subtitle>Page not found.</v-card-subtitle>
+    <v-card-actions>
+      <v-btn
+        text
+        link
+        to="/"
+      >
+        Go to homepage
+      </v-btn>
+    </v-card-actions>
+  </v-card>
 </template>
